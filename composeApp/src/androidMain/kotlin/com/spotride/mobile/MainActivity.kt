fd67e4d00ec870898e276708f84aff1f243a5cf9
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-11-28 22:03:03 +0100

    [ST-36] Packages organization

--- composeApp/src/androidMain/kotlin/com/spotride/mobile/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/spotride/mobile/MainActivity.kt
@@ -3,22 +3,13 @@ package com.spotride.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.spotride.mobile.ui.StartScreenPreview
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            SpotrideAppPreview()
+            StartScreenPreview()
         }
     }
-}
-
-@Preview
-@Composable
-fun SpotrideAppPreview() {
-    SpotrideApp(UserApiService())
 }
